8f4b35890d4fd22288888705041d89e0bafd3eb1
etmula <mountainroad@hotmail.co.jp>
2021-06-06 15:06:15 +0900

    Create decompress.sh

---
+++ decompress.sh
@@ -0,0 +1,11 @@
+echo Decompressing abstr/
+tar -I pbzip2 -xf abstr.tar.gz
+
+echo Decompressing fmbibs/
+tar -I pbzip2 -xf fmbibs.tar.gz
+
+echo Decompressing html/
+tar -I pbzip2 -xf html.tar.gz
+
+echo Decompressing mml/
+tar -I pbzip2 -xf mml.tar.gz
